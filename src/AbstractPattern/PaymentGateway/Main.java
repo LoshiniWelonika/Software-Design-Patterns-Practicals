84f1982518ee2020bbafe55c2423e7ca0317c108
LoshiniWelonika <welonikakedapanawala@gmail.com>
2026-04-22 10:55:39 +0530

    Abstract Pattern - Payment Gateway

---
+++ src/AbstractPattern/PaymentGateway/Main.java
@@ -0,0 +1,8 @@
+package AbstractPattern.PaymentGateway;
+
+public class Main {
+    public static void main(String[] args) {
+        Payment payment = new PaymentAdapter(new PayPal());
+        payment.pay(5000);
+    }
+}
---
+++ src/AbstractPattern/PaymentGateway/PayPal.java
@@ -0,0 +1,7 @@
+package AbstractPattern.PaymentGateway;
+
+class PayPal {
+    public void sendPayment(double amount){
+        System.out.println("Paid using Paypal: " + amount);
+    }
+}
