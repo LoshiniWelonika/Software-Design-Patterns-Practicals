80a5d9ab2132b3c33a4be653aeb509915fe4ea0b
LoshiniWelonika <welonikakedapanawala@gmail.com>
2026-05-11 10:41:33 +0530

    Strategy Pattern - Game Character Attack

---
+++ src/StrategyPattern/GameCharacterAttack/AttackMethods.java
@@ -0,0 +1,19 @@
+package StrategyPattern.GameCharacterAttack;
+
+class SwordAttack implements AttackStrategy {
+    public void attack() {
+        System.out.println("Attacking with Sword");
+    }
+}
+
+class GunAttack implements AttackStrategy {
+    public void attack() {
+        System.out.println("Attacking with Gun");
+    }
+}
+
+class MagicAttack implements AttackStrategy {
+    public void attack() {
+        System.out.println("Attacking with Magic");
+    }
+}
---
+++ src/StrategyPattern/GameCharacterAttack/Main.java
@@ -0,0 +1,13 @@
+package StrategyPattern.GameCharacterAttack;
+
+public class Main {
+    public static void main(String[] args) {
+        GameCharacter player = new GameCharacter();
+
+        player.setAttackStrategy(new SwordAttack());
+        player.performAttack();
+
+        player.setAttackStrategy(new MagicAttack());
+        player.performAttack();
+    }
+}
